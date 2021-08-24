b23ee952927db15f0e76ce452dc5e643803d9ed7
Leo Febba <leonardofebba@hotmail.com>
2021-08-24 18:25:33 -0300

    Primeiro commit

---
+++ js/debounce.js
@@ -0,0 +1,20 @@
+export default function debounceScroll(){
+    function debounce(callback, delay){
+        let timer;
+        return(...args)=>{
+            if(timer){
+                clearTimeout(timer);
+            }
+            timer = setTimeout(()=>{
+                callback(...args);
+                timer = null;
+            }, delay)
+        }
+    }
+
+    function onScroll(){
+        console.log('teste');
+    }
+    const debouncedScroll = debounce(onScroll, 200);
+    window.addEventListener('scroll', debouncedScroll);
+}
